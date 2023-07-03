014d6c38439c766c281cfb0c0785c1e4afa6bc83
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 12:53:31 +0100

    [ADD] Draw a circle.

--- draw.py
+++ draw.py
@@ -16,4 +16,8 @@ cv.rectangle(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thi
 # cv.rectangle(blank, (0,0), (250,500), (0,255,0), thickness=-1)
 cv.imshow('Rectangle', blank)
 
+# 3. Draw a Circle
+cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 40, (0,0,255), thickness=3)
+cv.imshow('Circle', blank)
+
 cv.waitKey(0)
