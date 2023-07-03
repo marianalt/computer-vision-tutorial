6c8872378713d966601cf6e3f6f75412780283e8
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 14:19:29 +0100

    [UPDATE] Increase blur.

--- basic.py
+++ basic.py
@@ -8,7 +8,7 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
 # Blur - gauss
-blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
 
 cv.waitKey(0)
