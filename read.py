8853cbd28cf3786669fbdd55cf8238aa71a17735
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 10:36:51 +0100

    [UPDATE] See results of bigger image.

--- read.py
+++ read.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 
 #to read an image, returns the matrix of pixels
-img = cv.imread('Photos/kitten.jpg')
+img = cv.imread('Photos/Kitty.jpg')
 
 #to show the matrix of pixels
 '''
 str - name of the window
 var - matrix of pixels to be shown
 '''
-cv.imshow('kitten', img)
+cv.imshow('Kitty', img)
 
 #waiting a delay seconds or a key to be pressed
 '''in this case, wait 0 seconds, so wait infinitely for a key to be pressed'''
