1b3697fb80c46e51f214cec370fb5aceda43e133
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 12:46:35 +0100

    [ADD] Draw rectangle.

--- draw.py
+++ draw.py
@@ -6,12 +6,12 @@ import numpy as np
 blank = np.zeros((500, 500, 3), dtype='uint8')
 cv.imshow('Blank', blank)
 
-# 1. paint the image a certain color - RGB => BGR
-blank[:] = 0,255,0
-cv.imshow('Green', blank)
-blank[:] = 0,0,255
-cv.imshow('Red', blank)
-blank[:] = 255,0,0
-cv.imshow('Blue', blank)
+# 1. Paint the image a certain color - RGB => BGR
+# blank[200:300, 300:400] = 0,255,0
+# cv.imshow('Green', blank)
+
+# 2. Draw a Rectangle
+cv.rectangle(blank, (0,0), (250,250), (0,255,0), thickness=2)
+cv.imshow('Rectangle', blank)
 
 cv.waitKey(0)
