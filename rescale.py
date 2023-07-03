fba5ddd5c2aa292f0984dfdc541358ecf1c33bae
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 12:09:31 +0100

    [ADD] Note.

--- rescale.py
+++ rescale.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 
 def rescaleFrame(frame, scale=0.75):
+    # Images, Videos and Live Video
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     dimensions = (width, height)
