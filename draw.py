d16585b52cff211c8ffed4b5f70971eddf9e2170
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 13:03:00 +0100

    Update draw.py

--- draw.py
+++ draw.py
@@ -11,18 +11,18 @@ cv.imshow('Blank', blank)
 # cv.imshow('Green', blank)
 
 # 2. Draw a Rectangle
-# cv.rectangle(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thickness=cv.FILLED)
-# #same as: 
-# # cv.rectangle(blank, (0,0), (250,500), (0,255,0), thickness=-1)
-# cv.imshow('Rectangle', blank)
+cv.rectangle(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thickness=cv.FILLED)
+#same as: 
+# cv.rectangle(blank, (0,0), (250,500), (0,255,0), thickness=-1)
+cv.imshow('Rectangle', blank)
 
-# # 3. Draw a Circle
-# cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 40, (0,0,255), thickness=-1)
-# cv.imshow('Circle', blank)
+# 3. Draw a Circle
+cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 40, (0,0,255), thickness=-1)
+cv.imshow('Circle', blank)
 
-# # 4. Draw a Line
-# cv.line(blank, (100,250), (300,400), (255,255,255), thickness=3)
-# cv.imshow('Line', blank)
+# 4. Draw a Line
+cv.line(blank, (100,250), (300,400), (255,255,255), thickness=3)
+cv.imshow('Line', blank)
 
 # 5. Write text
 cv.putText(blank, 'Hello', (225,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
