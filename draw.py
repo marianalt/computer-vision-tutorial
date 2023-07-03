fbc521640cbfbf68d58150ef1725c0dfc349f5e6
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 12:46:57 +0100

    [UPDATE] Longer rectangle.

--- draw.py
+++ draw.py
@@ -11,7 +11,7 @@ cv.imshow('Blank', blank)
 # cv.imshow('Green', blank)
 
 # 2. Draw a Rectangle
-cv.rectangle(blank, (0,0), (250,250), (0,255,0), thickness=2)
+cv.rectangle(blank, (0,0), (250,500), (0,255,0), thickness=2)
 cv.imshow('Rectangle', blank)
 
 cv.waitKey(0)
