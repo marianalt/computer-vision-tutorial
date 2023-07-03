edc0a5ab2c3ca3aafdbd81817b01e0fb1be5a8c5
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 14:43:21 +0100

    [ADD] Cropping.

--- basic.py
+++ basic.py
@@ -24,7 +24,12 @@ cv.imshow('Cat', img)
 # cv.imshow('Eroded', eroded)
 
 # Resize
-resized = cv.resize(img, (800,800), interpolation=cv.INTER_CUBIC) #useful to scale the image, slowest but has more quality
-cv.imshow('Resized', resized)
+# resized = cv.resize(img, (800,800), interpolation=cv.INTER_CUBIC) #useful to scale the image, slowest but has more quality
+# cv.imshow('Resized', resized)
+
+# Cropping - array slicing
+cropped = img[50:200, 200:400]
+cv.imshow('Cropped', cropped)
+
 
 cv.waitKey(0)
