28ff75204158620b91d0b210a3bc63aaa3ceddf6
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 11:46:02 +0100

    [ADD] Rescale videos.

--- read.py
+++ read.py
@@ -1,32 +1,21 @@
 import cv2 as cv
 
 # Reading images -------------------------
-
 '''
 img = cv.imread('Photos/kitten.jpg')
 cv.imshow('kitten', img)
 '''
 
 # Reading videos -------------------------
-
-#str or int - path or camera (0/1/2)
-# returns pointer
 capture = cv.VideoCapture('Videos/dog.mp4')
 
-#use a while loop and read video frame by frame
 while True:
-    '''
-    bool - is frame was successfully read or not
-    frame - frame read
-    '''
     isTrue, frame = capture.read()
     cv.imshow('Video', frame)
     
-    #to every frame read, see if 'd' key was pressed
+    #see if 'd' key was pressed
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
 
-#if having error -215, the path is probably wrong or there are no frames left to be shown
-#basically, there is nothing to be shown (for images or videos)
 capture.release()
 cv.destroyAllWindows()
---
+++ rescale.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+capture = cv.VideoCapture('Videos/kelly.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+
+    frame_resized = rescaleFrame(frame, 0.1)
+    #frame_resized = rescaleFrame(frame, scale=0.2)
+
+
+    cv.imshow('Video', frame)
+    cv.imshow('Video resized', frame_resized)
+    
+    #see if 'd' key was pressed
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
