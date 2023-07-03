7c9395da6c5676744f183859640b618fc457659a
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 12:56:40 +0100

    [ADD] Draw line.

--- draw.py
+++ draw.py
@@ -17,7 +17,11 @@ cv.rectangle(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thi
 cv.imshow('Rectangle', blank)
 
 # 3. Draw a Circle
-cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 40, (0,0,255), thickness=3)
+cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 40, (0,0,255), thickness=-1)
 cv.imshow('Circle', blank)
+
+# 4. Draw a Line
+cv.line(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (255,255,255), thickness=3)
+cv.imshow('Line', blank)
 
 cv.waitKey(0)
