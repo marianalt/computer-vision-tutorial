4ac4b808b3f5e01b13c431b448ee735db4f8d07f
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 14:25:52 +0100

    [ADD] Canny edge detector method.

--- basic.py
+++ basic.py
@@ -7,8 +7,12 @@ cv.imshow('Cat', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
-# Blur - gauss
+# Blur - Gauss blur method
 blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
+
+# Edge Cascade - Canny edge detector
+canny = cv.Canny(img, 125, 175)
+cv.imshow('Canny Edges', canny)
 
 cv.waitKey(0)
