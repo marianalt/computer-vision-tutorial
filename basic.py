aee931a4b3df8a5d9c50c6523d0a1d1620cd5694
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 14:19:10 +0100

    [ADD] Blur.

--- basic.py
+++ basic.py
@@ -1,10 +1,14 @@
 import cv2 as cv
 
-img = cv.imread('Photos/cat.jpg')
+img = cv.imread('Photos/street.jpg')
 cv.imshow('Cat', img)
 
 # Converting to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
+
+# Blur - gauss
+blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
 
 cv.waitKey(0)
