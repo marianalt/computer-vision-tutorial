460bc3d1fa6fff00109762ae4e7fcefedc55440b
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-03 14:38:15 +0100

    [ADD] Interpolations in resizing.

--- basic.py
+++ basic.py
@@ -3,28 +3,28 @@ import cv2 as cv
 img = cv.imread('Photos/street.jpg')
 cv.imshow('Cat', img)
 
-# Converting to grayscale
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+# # Converting to grayscale
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
 
-# Blur - Gauss blur method
-blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
-cv.imshow('Blur', blur)
+# # Blur - Gauss blur method
+# blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+# cv.imshow('Blur', blur)
 
-# Edge Cascade - Canny edge detector
-canny = cv.Canny(blur, 125, 175)
-cv.imshow('Canny Edges', canny)
+# # Edge Cascade - Canny edge detector
+# canny = cv.Canny(blur, 125, 175)
+# cv.imshow('Canny Edges', canny)
 
-# Dilating the image
-dilated = cv.dilate(canny, (7,7), iterations=3)
-cv.imshow('Dilated', dilated)
+# # Dilating the image
+# dilated = cv.dilate(canny, (7,7), iterations=3)
+# cv.imshow('Dilated', dilated)
 
-# Eroding
-eroded = cv.erode(dilated, (7,7), iterations=3)
-cv.imshow('Eroded', eroded)
+# # Eroding
+# eroded = cv.erode(dilated, (7,7), iterations=3)
+# cv.imshow('Eroded', eroded)
 
 # Resize
-resized = cv.resize(img, (500,500)) #resizing ignoring aspect ratio
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_AREA) #useful to shrink image
 cv.imshow('Resized', resized)
 
 cv.waitKey(0)
