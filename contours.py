f68b6903cbd8184be557681896f7a759d32cc8ef
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-05 16:10:48 +0100

    [ADD] Draw contours in an image.

--- contours.py
+++ contours.py
@@ -1,8 +1,11 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread('Photos/street.jpg')
-
 cv.imshow('Street', img)
+
+blank = np.zeros(img.shape, dtype='uint8')
+cv.imshow('Blank', blank)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
@@ -21,5 +24,8 @@ cv.imshow('Thresh', thresh)
 # CHAIN_APPROX_NONE - contour approximation method, all of them. SIMPLE - compress some of them in one
 contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 print(f'{len(contours)} contour(s) found!')
+
+cv.drawContours(blank, contours, -1, (0,0, 255), 2)
+cv.imshow('Contours drawn', blank)
 
 cv.waitKey(0)
