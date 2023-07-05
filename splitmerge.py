83123cfbf64c006d089a2d652791d1c03f00a5fe
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-05 18:09:29 +0100

    [ADD] Notes.

--- splitmerge.py
+++ splitmerge.py
@@ -6,8 +6,10 @@ cv.imshow('Street', img)
 
 blank = np.zeros(img.shape[:2], dtype='uint8')
 
+# Split an image into its respective color channels
 b, g, r = cv.split(img)
 
+# Reconstruct the image to display the actual color involved in that channel
 blue = cv.merge([b, blank, blank])
 green = cv.merge([blank, g, blank])
 red = cv.merge([blank, blank, r])
@@ -26,6 +28,7 @@ print(b.shape)
 print(g.shape)
 print(r.shape)
 
+# Merge the color channels back to its original image
 merged = cv.merge([b,g,r])
 cv.imshow('Merged', merged)
 
