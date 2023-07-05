fbdc34f1f488689ce164e831233aceeb4ce2d239
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2023-07-05 17:27:14 +0100

    [ADD] Split method with shapes.

---
+++ splitmerge.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Photos/countryside.jpg')
+cv.imshow('Street', img)
+
+b, g, r = cv.split(img)
+
+#is shown gray cause have a shape of one
+cv.imshow('Blue', b)
+cv.imshow('Green', g)
+cv.imshow('Red', r)
+
+print(img.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
+cv.waitKey(0)
